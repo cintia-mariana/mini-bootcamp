e5249329f301869155e77d485b5fd78f67ec65b6
chazarreta <cintia.chazarreta@accenture.com>
2022-09-15 10:16:33 -0300

    seccion 7

---
+++ Training/directives-start (1)/directives-start/src/app/basic-hightlight/basic-hightlight.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, OnInit } from "@angular/core";
+
+@Directive({
+    selector: '[appBasicHightlight]'
+})
+export class BasicHightlightDirective implements OnInit {
+    constructor (private elementRef: ElementRef) {
+       }
+       ngOnInit(): void {
+           this.elementRef.nativeElement.style.backgroundColor= 'green';
+       }
+
+}
