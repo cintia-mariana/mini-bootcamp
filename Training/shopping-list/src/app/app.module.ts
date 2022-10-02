9874063abed171abb5a894141d991094592ed32e
chazarreta <cintia.chazarreta@accenture.com>
2022-10-02 15:33:36 -0300

    seccion 20 y 21

--- Training/shopping-list/src/app/app.module.ts
+++ Training/shopping-list/src/app/app.module.ts
@@ -13,12 +13,17 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 
 @NgModule({
@@ -34,6 +39,10 @@ import { RecipeService } from './recipes/recipe.service';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    AuthComponent,
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -43,7 +52,7 @@ import { RecipeService } from './recipes/recipe.service';
     HttpClientModule,
    
   ],
-  providers: [ShoppingListService,RecipeService],
+  providers: [ShoppingListService,RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Training/shopping-list/src/app/shared/data-storage.service.ts
+++ Training/shopping-list/src/app/shared/data-storage.service.ts
@@ -1,38 +1,40 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import {Recipe} from "../recipes/recipe.model";
-import {map, tap} from "rxjs/operators";
+import {exhaustMap, map, take, tap} from "rxjs/operators";
 
 import { RecipeService } from "../recipes/recipe.service";
+import { AuthService } from "../auth/auth.service";
 
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipesService: RecipeService){}
+    constructor(private http: HttpClient, private recipesService: RecipeService, private authService: AuthService){}
 
-        storeRecipes(){
-            const recipes= this.recipesService.getRecipes();
-            this.http.put('https://ng-course-recipe-book-29498-default-rtdb.firebaseio.com/recipes.json', recipes)
-            .subscribe(response =>{
-                console.log(response);
+    storeRecipes(){
+        const recipes= this.recipesService.getRecipes();
+        this.http.put('https://ng-course-recipe-book-29498-default-rtdb.firebaseio.com/recipes.json', recipes)
+        .subscribe(response =>{
+            console.log(response);
 
-                
-            });
-        }
+            
+        });
+    }
+
     fetchRecipes(){
         return this.http
-        .get<Recipe[]>('https://ng-course-recipe-book-29498-default-rtdb.firebaseio.com/recipes.json')
-        .pipe(map(recipes=>{
-            return recipes.map(recipe => {
-                return {
-                    ...recipe,
-                    ingredients: recipe.ingredients ? recipe.ingredients : []};
-            });
-        }),
-        tap(recipes => {
-            this.recipesService.setRecipes(recipes);
-        })
-        )
-        
+            .get<Recipe[]>('https://ng-course-recipe-book-29498-default-rtdb.firebaseio.com/recipes.json')
+            .pipe(
+                map(recipes=>{
+                    return recipes.map(recipe => {
+                        return {
+                            ...recipe,
+                            ingredients: recipe.ingredients ? recipe.ingredients : []};
+                    });
+                }),
+                tap(recipes => {
+                    this.recipesService.setRecipes(recipes);
+                })            
+            );        
     }
 }
